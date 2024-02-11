b00da5ccadc6179fcec2ecd4025c986b69858927
paco7828 <koszegipatrik888@gmail.com>
2024-02-11 22:44:10 +0100

    Final commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Comp() {
+    const [inputs, setInputs] = useState({});
+    const [submitText, setSubmitText] = useState("Export div");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setInputs(prevValues => ({ ...prevValues, [name]: value }));
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSubmitText("Exported in console!");
+        console.table(divStyle);
+    }
+
+    const divStyle = {
+        transition: "all 0.2s",
+        width: `${inputs.width}px`,
+        height: `${inputs.height}px`,
+        backgroundColor: inputs.backgroundColor,
+        border: `${inputs.borderWidth}px ${inputs.borderType} ${inputs.borderColor}`,
+        borderRadius: `${inputs.borderRadius}%`
+    }
+
+    Object.keys(divStyle).forEach(key => {
+        if (typeof divStyle[key] === "undefined" || divStyle[key].includes("undefined")) {
+            delete divStyle[key];
+        }
+    });
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input type="number" name="width" placeholder="Width in pixels..." onChange={handleChange} />
+                <br />
+                <input type="number" name="height" placeholder="Height in pixels..." onChange={handleChange} />
+                <br />
+                <input type="text" name="backgroundColor" placeholder="Background color..." onChange={handleChange} />
+                <br />
+                <input type="number" name="borderWidth" placeholder="Border width..." onChange={handleChange} />
+                <br />
+                <input type="text" name="borderType" placeholder="Border type..." onChange={handleChange} />
+                <br />
+                <input type="text" name="borderColor" placeholder="Border color..." onChange={handleChange} />
+                <br />
+                <input type="text" name="borderRadius" placeholder="Border radius in %..." onChange={handleChange} />
+                <br />
+                <input type="submit" value={submitText} />
+            </form>
+            <div style={divStyle}></div>
+        </>
+    );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Comp />);
